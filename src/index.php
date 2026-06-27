c6055fcfdcd6452bf5a47c56750e3b0ec80b24dd
root <root@ip-172-31-9-22.ap-southeast-2.compute.internal>
2026-06-27 11:16:13 +0000

    intex update

--- src/index.php
+++ src/index.php
@@ -1,2 +1,11 @@
 <?php
-phpinfo();
+// sample.php
+
+$name = "Hariom";
+
+echo "<h1>Hello, $name!</h1>";
+echo "<p>Welcome to PHP programming.</p>";
+
+$currentDate = date("Y-m-d H:i:s");
+echo "<p>Current Date and Time: $currentDate</p>";
+?>
