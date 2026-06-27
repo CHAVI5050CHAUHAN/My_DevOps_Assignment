b73544f100d5a7039487b60f7ef47819b6ba6736
CHAVI5050CHAUHAN <109215678+CHAVI5050CHAUHAN@users.noreply.github.com>
2026-06-28 00:25:09 +0530

    Update config.php

--- src/config.php
+++ src/config.php
@@ -1,8 +1,8 @@
 <?php
 define("DB_SERVER", "mysql");
-define("DB_USERNAME", "root");
-define("DB_PASSWORD", "root123");
-define("DB_NAME", "assignmentdb");
+define("DB_USERNAME", "app_user");
+define("DB_PASSWORD", "app_pass");
+define("DB_NAME", "app_db");
 
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 
